6be57e37a6bc288b4c4aaf9b564976a9be1fd775
Folkert van Heusden <mail@vanheusden.com>
2023-12-14 21:32:43 +0100

    split()

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
+#include <string>
 #include <unistd.h>
+#include <vector>
 
 
 ssize_t READ(const int fd, uint8_t *whereto, size_t len)
@@ -35,4 +37,34 @@ ssize_t WRITE(const int fd, const uint8_t *whereto, size_t len)
 	}
 
 	return cnt;
+}
+
+std::vector<std::string> split(std::string in, std::string splitter)
+{
+	std::vector<std::string> out;
+	size_t splitter_size = splitter.size();
+
+	for(;;)
+	{
+		size_t pos = in.find(splitter);
+		if (pos == std::string::npos)
+			break;
+
+		std::string before = in.substr(0, pos);
+		out.push_back(before);
+
+		size_t bytes_left = in.size() - (pos + splitter_size);
+		if (bytes_left == 0)
+		{
+			out.push_back("");
+			return out;
+		}
+
+		in = in.substr(pos + splitter_size);
+	}
+
+	if (in.size() > 0)
+		out.push_back(in);
+
+	return out;
 }
--- utils.h
+++ utils.h
@@ -1,2 +1,7 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+
 ssize_t READ (const int fd, uint8_t *whereto, size_t len);
 ssize_t WRITE(const int fd, const uint8_t *whereto, size_t len);
+std::vector<std::string> split(std::string in, std::string splitter);
