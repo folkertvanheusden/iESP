71c341319ec42a76bee29aa10baf292e5e305061
folkert van heusden <mail@vanheusden.com>
2024-01-10 22:02:29 +0100

    linux mode: log to disk

--- log.h
+++ log.h
@@ -1,7 +1,12 @@
 #ifdef linux
 #define DOLOG(fmt, ...) do {                \
+		FILE *fh = fopen("log.dat", "a+"); \
+		if (fh) {                   \
+			fprintf(fh, fmt, ##__VA_ARGS__); \
+			fclose(fh);         \
+		}                           \
                 printf(fmt, ##__VA_ARGS__); \
         } while(0)
 #else
-#define DOLOG(ll, always, fmt, ...) do { } while(0)
+#define DOLOG(ll, fmt, ...) do { } while(0)
 #endif
