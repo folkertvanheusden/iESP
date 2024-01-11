9d32111d0030c0ad23f9b76df8b159801b54cd13
folkert van heusden <mail@vanheusden.com>
2024-01-11 14:42:59 +0100

    get_lba_status

--- scsi.cpp
+++ scsi.cpp
@@ -26,6 +26,7 @@ std::optional<scsi_response> scsi::send(const uint8_t *const CDB, const size_t s
 	if (opcode == o_test_unit_ready) {
 	}
 	else if (opcode == o_inquiry) {
+		DOLOG("scsi::send: inquiry\n");
 		response.data.second = 36;
 		response.data.first = new uint8_t[response.data.second]();
 		response.data.first[0] = 0;  // disk
@@ -41,6 +42,7 @@ std::optional<scsi_response> scsi::send(const uint8_t *const CDB, const size_t s
 		memcpy(&response.data.first[32], "1.0", 3);  // TODO
 	}
 	else if (opcode == o_read_capacity) {
+		DOLOG("scsi::send: read_capacity\n");
 		response.data.second = 8;
 		response.data.first = new uint8_t[response.data.second]();
 		response.data.first[0] = 0;  // 256 sectors
@@ -52,11 +54,31 @@ std::optional<scsi_response> scsi::send(const uint8_t *const CDB, const size_t s
 		response.data.first[6] = 0x02;
 		response.data.first[7] = 0x00;
 	}
+	else if (opcode == o_get_lba_status) {
+		uint64_t starting_lba = (uint64_t(CDB[2]) << 56) | (uint64_t(CDB[3]) << 48) | (uint64_t(CDB[4]) << 40) | (uint64_t(CDB[5]) << 32) | (CDB[6] << 24) | (CDB[7] << 16) | (CDB[8] << 8) | CDB[9];
+		uint32_t allocation_length = (CDB[10] << 24) | (CDB[11] << 16) | (CDB[12] << 8) | CDB[13];
+		DOLOG("scsi::send: get_lba_status(CDB size: %zu), starting lba %llu, allocation length: %u\n", size, starting_lba, allocation_length);
+
+		response.data.second = 24;
+		response.data.first = new uint8_t[response.data.second]();
+		response.data.first[0] = 0;  // parameter length
+		response.data.first[1] = 0;
+		response.data.first[2] = 0;
+		response.data.first[3] = 16;
+		for(int i=2; i<10; i++)  // LBA status logical block address
+			response.data.first[i - 2 + 8] = CDB[i];
+		int blocks_required = (allocation_length + 4095) / 4096;
+		response.data.first[16] = blocks_required >> 24;
+		response.data.first[17] = blocks_required >> 16;
+		response.data.first[18] = blocks_required >>  8;
+		response.data.first[19] = blocks_required;
+	}
 	else if (opcode == o_write_16) {
 		uint64_t lba = (uint64_t(CDB[2]) << 56) | (uint64_t(CDB[3]) << 48) | (uint64_t(CDB[4]) << 40) | (uint64_t(CDB[5]) << 32) | (CDB[6] << 24) | (CDB[7] << 16) | (CDB[8] << 8) | CDB[9];
 		uint32_t transfer_length = (CDB[10] << 24) | (CDB[11] << 16) | (CDB[12] << 8) | CDB[13];
 
 		DOLOG("scsi::send: write_16(CDB size: %zu), offset %llu, %u sectors\n", size, lba, transfer_length);
+		// TODO
 	}
 	else {
 		DOLOG("scsi::send: opcode %02xh not implemented\n", opcode);
--- scsi.h
+++ scsi.h
@@ -26,6 +26,7 @@ public:
 		o_inquiry         = 0x12,
 		o_read_capacity   = 0x25,
 		o_write_16        = 0x8a,
+		o_get_lba_status  = 0x9e,
 	};
 
 	std::optional<scsi_response> send(const uint8_t *const CDB, const size_t size);
