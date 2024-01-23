2c1ef86904bd0a56bc6c8aaf9dd08203fb50cfd3
Folkert van Heusden <mail@vanheusden.com>
2024-01-23 22:31:45 +0100

    correct sense data for inquiry with unknown page code

--- scsi.cpp
+++ scsi.cpp
@@ -4,6 +4,7 @@
 
 #include "log.h"
 #include "scsi.h"
+#include "utils.h"
 
 
 scsi::scsi()
@@ -20,27 +21,50 @@ std::optional<scsi_response> scsi::send(const uint8_t *const CDB, const size_t s
 
 	scsi_opcode opcode = scsi_opcode(CDB[0]);
 	DOLOG("SCSI opcode: %02xh, CDB size: %zu\n", opcode, size);
+#ifdef linux
+	DOLOG("CDB contents: %s\n", to_hex(CDB, size).c_str());
+#endif
 
 	scsi_response   response { };
 	response.type = ir_as_is;
 
 	if (opcode == o_test_unit_ready) {
 	}
-	else if (opcode == o_inquiry) {
+	else if (opcode == o_inquiry) {  // 0x12
 		DOLOG("scsi::send: INQUIRY\n");
-		response.data.second = 36;
-		response.data.first = new uint8_t[response.data.second]();
-		response.data.first[0] = 0;  // disk
-		response.data.first[1] = 0;  // not removable
-		response.data.first[2] = 4;  // VERSION
-		response.data.first[3] = 2;  // response data format
-		response.data.first[4] = response.data.second - 4;  // additional length
-		response.data.first[5] = 0;
-		response.data.first[6] = 0;
-		response.data.first[7] = 0;
-		memcpy(&response.data.first[8],  "vnHeusdn", 8);
-		memcpy(&response.data.first[16], "iESP", 4);  // TODO
-		memcpy(&response.data.first[32], "1.0", 3);  // TODO
+		if (CDB[1] & 1) {
+			DOLOG(" INQUIRY: EVPD\n");
+			DOLOG(" INQUIRY: PageCode: %02xh\n", CDB[2]);
+		}
+		if (CDB[1] & 2)
+			DOLOG(" INQUIRY: CmdDt\n");
+		DOLOG(" INQUIRY: AllocationLength: %d\n", (CDB[3] << 8) | CDB[4]);
+		DOLOG(" INQUIRY: ControlByte: %02xh\n", CDB[5]);
+		if ((CDB[1] & 1) == 0) {
+			response.data.second = 36;
+			response.data.first = new uint8_t[response.data.second]();
+			response.data.first[0] = 0;  // disk
+			response.data.first[1] = 0;  // not removable
+			response.data.first[2] = 4;  // VERSION
+			response.data.first[3] = 2;  // response data format
+			response.data.first[4] = response.data.second - 4;  // additional length
+			response.data.first[5] = 0;
+			response.data.first[6] = 0;
+			response.data.first[7] = 0;
+			memcpy(&response.data.first[8],  "vnHeusdn", 8);
+			memcpy(&response.data.first[16], "iESP", 4);  // TODO
+			memcpy(&response.data.first[32], "1.0", 3);  // TODO
+		}
+		else {  // PageCode not supported
+			response.sense_data = {
+				0x72,  // current errors
+				0x05,  // key: illegal request
+				0x24,  // ASC: invalid field in cdb
+				0x00,  // ASQ: -
+				0x00, 0x00, 0x00,  // reserved
+				0x00  // additional sense length
+			};
+		}
 	}
 	else if (opcode == o_read_capacity_10) {
 		DOLOG("scsi::send: READ_CAPACITY\n");
