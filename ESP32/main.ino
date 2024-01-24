74ace2dde4958516961d53a123c6838d3a9554d7
Folkert van Heusden <mail@vanheusden.com>
2024-01-24 01:15:49 +0100

    wifimanager

--- ESP32/main.ino
+++ ESP32/main.ino
@@ -5,12 +5,23 @@
 #include <cstdio>
 #include <SPI.h>
 #include <LittleFS.h>
+#include <WiFiManager.h>
 
 #include "../server.h"
+
+
+WiFiManager wifiManager;
 
 void setup()
 {
 	Serial.begin(115200);
+	Serial.println(F("iESP, (C) 2023-2024 by Folkert van Heusden <mail@vanheusden.com"));
+
+	wifiManager.setConfigPortalTimeout(120);
+	wifiManager.autoConnect();
+
+	Serial.print(F("Will listen on (in a few seconds): "));
+	Serial.println(WiFi.localIP());
 }
 
 void loop()
