6699496d04dc40aeabf4759294f5ef4997890805
folkert van heusden <mail@vanheusden.com>
2024-02-01 19:14:15 +0100

    step_start

--- test-io.py
+++ test-io.py
@@ -6,11 +6,14 @@ from pyscsi.pyscsi.scsi import SCSI
 from pyscsi.utils import init_device
 
 device = init_device('iscsi://192.168.64.206/test/0')
+# device = init_device('iscsi://192.168.65.245/test2/0')
+# device = init_device('iscsi://192.168.65.238/target1/0')
 bs = 4096
 count = 256
+step_start = 1
 
 with SCSI(device, blocksize=bs) as s:
-    for step in range(1, count):
+    for step in range(step_start, count):
         print(f'Step: {step}')
         print('  write')
         for i in range(0, count, step):
@@ -19,10 +22,7 @@ with SCSI(device, blocksize=bs) as s:
 
         print('  read & verify')
         for i in range(0, count, step):
-            r = s.read16(
-                i,
-                step,
-            ).datain
+            r = s.read16(i, step).datain
 
             for t in range(0, count):
                 if r[t] != i:
