0180731ce73daac516a932e7ac0e31f189b15f7d
folkert van heusden <mail@vanheusden.com>
2024-02-02 10:40:00 +0100

    - need to SBC to tgt - storage is at lun 1, not 0: 0 is the controller, see "tgtadm --mode target --op show" - fix for size of data block

--- test-io.py
+++ test-io.py
@@ -2,28 +2,37 @@
 
 # https://github.com/python-scsi/python-scsi/
 
+from pyscsi.pyscsi.scsi_cdb_read16 import Read16
+from pyscsi.pyscsi.scsi_cdb_write16 import Write16
+from pyscsi.pyscsi.scsi_enum_command import sbc
 from pyscsi.pyscsi.scsi import SCSI
 from pyscsi.utils import init_device
 
-device = init_device('iscsi://192.168.64.206/test/0')
-# device = init_device('iscsi://192.168.65.245/test2/0')
+# device = init_device('iscsi://192.168.64.206/test/0')
+device = init_device('iscsi://192.168.65.245/test/1')
 # device = init_device('iscsi://192.168.65.238/target1/0')
 bs = 4096
 count = 256
 step_start = 1
+errors = False
+with SCSI(device, blocksize=bs) as d:
+    d.device.opcodes = sbc
 
-with SCSI(device, blocksize=bs) as s:
     for step in range(step_start, count):
         print(f'Step: {step}')
         print('  write')
+        data = bytearray([ 0 ] * bs * step)
         for i in range(0, count, step):
-            data = bytearray([ i ] * bs)
-            w = s.write16(i, step, data).dataout
+            data[0] = i
+            w = d.write16(i, step, data).dataout
 
         print('  read & verify')
         for i in range(0, count, step):
-            r = s.read16(i, step).datain
+            r = d.read16(i, step).datain
 
-            for t in range(0, count):
-                if r[t] != i:
-                    print(r[t], i, r[t] == i)
+            if r[0] != i:
+                print(r[t], i, r[t] == i)
+                errors = True
+
+if not errors:
+    print('All good!')
