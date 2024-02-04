5193934f70507395c3c9a9d8a40e0665f53bfb0f
Folkert van Heusden <mail@vanheusden.com>
2024-02-04 17:51:54 +0100

    mDNS & hostname

--- ESP32/main.ino
+++ ESP32/main.ino
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <csignal>
 #include <cstdio>
+#include <ESPmDNS.h>
 #include <SPI.h>
 #include <LittleFS.h>
 #include <WiFiManager.h>
@@ -14,16 +15,29 @@
 
 std::atomic_bool stop { false };
 
-WiFiManager wifiManager;
+WiFiManager *wifiManager { nullptr };
 
 void setup()
 {
 	Serial.begin(115200);
+	Serial.setDebugOutput(true);
+	WiFi.hostname("iESP");
+
 	Serial.println(F("iESP, (C) 2023-2024 by Folkert van Heusden <mail@vanheusden.com>"));
 	Serial.println(F("Compiled on " __DATE__ " " __TIME__));
 
-	wifiManager.setConfigPortalTimeout(120);
-	wifiManager.autoConnect();
+	const int cpu_freq = 240;
+	Serial.printf("CPU frequency was %d, now setting to %d\r\n", getCpuFrequencyMhz(), cpu_freq);
+	setCpuFrequencyMhz(cpu_freq);
+
+	wifiManager = new WiFiManager();
+	wifiManager->setConfigPortalTimeout(120);
+	wifiManager->autoConnect();
+
+	if (MDNS.begin("iESP"))
+		MDNS.addService("iscsi", "tcp", 3260);
+	else
+		Serial.println(F("Failed starting mdns responder"));
 
 	Serial.print(F("Will listen on (in a bit): "));
 	Serial.println(WiFi.localIP());
