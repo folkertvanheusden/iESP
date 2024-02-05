49394b3307374affa866fd20b377c727b0bdc9c9
Folkert van Heusden <mail@vanheusden.com>
2024-02-05 13:09:13 +0100

    gettimeofday is obsolete

--- log.h
+++ log.h
@@ -6,12 +6,13 @@
 #define DOLOG(fmt, ...) do {                \
 		FILE *fh = fopen("log.dat", "a+"); \
 		if (fh) {                          \
-			timeval tv;          \
-			gettimeofday(&tv, nullptr);\
-			struct tm tm;        \
-			localtime_r(&tv.tv_sec, &tm);\
-			fprintf(fh, "%04d-%02d-%02d %02d:%02d:%02d.%06d ", \
-				tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, int(tv.tv_usec)); \
+			timespec ts;          \
+			if (clock_gettime(CLOCK_REALTIME, &ts) == 0) { \
+				struct tm tm;        \
+				localtime_r(&ts.tv_sec, &tm);\
+				fprintf(fh, "%04d-%02d-%02d %02d:%02d:%02d.%06d ", \
+				tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, int(ts.tv_nsec / 1000)); \
+			} \
 			fprintf(fh, fmt, ##__VA_ARGS__); \
 			fclose(fh);         \
 		}                           \
