6bcc6f453e7fe93cfef9ddec51e4ae0ab9fbe194
folkert van heusden <mail@vanheusden.com>
2024-02-09 21:28:41 +0100

    allow i/o transactions of more than 1 block

--- test-blockdevice.py
+++ test-blockdevice.py
@@ -6,8 +6,11 @@ import random
 import sys
 import time
 
-dev = sys.argv[1]
-blocksize = int(sys.argv[2])
+dev = sys.argv[1]  # device file
+blocksize = int(sys.argv[2])  # size of each block (512, 4096, etc)
+max_b = int(sys.argv[3])  # max. number of blocks in one go
+
+random.seed()
 
 seed = int(time.time())
 fd = os.open(dev, os.O_RDWR)
@@ -24,6 +27,7 @@ def gen_block(size, offset, seed2):
         m = hashlib.md5(m).digest()
     return out
 
+total_n = 0
 n = 0
 verified = 0
 start = time.time()
@@ -31,27 +35,43 @@ prev = start
 w = 0
 
 while True:
-    offset = random.randint(0, dev_size) & ~(blocksize - 1)
+    cur_n_blocks = random.randint(1, max_b)
+    offset = random.randint(0, dev_size - blocksize * cur_n_blocks) & ~(blocksize - 1)
     nr = offset // blocksize
 
-    if seen[nr] != None:
-        # verify
-        b = gen_block(blocksize, offset, seen[nr])
+    b = []
+    has_none = False
+    for i in range(0, cur_n_blocks):
+        if seen[nr + i] == None:
+            has_none = True
+            break
 
-        data = os.pread(fd, blocksize, offset)
-        assert(data == b)
-        verified += 1
+    if has_none == False:
+        # verify
+        for i in range(0, cur_n_blocks):
+            b.append(gen_block(blocksize, offset + i * blocksize, seen[nr + i]))
+
+        data = os.pread(fd, blocksize * cur_n_blocks, offset)
+
+        for i in range(0, cur_n_blocks):
+            cur_b_offset = i * blocksize 
+            assert(data[cur_b_offset:cur_b_offset+blocksize] == b[i])
+
+            verified += 1
 
     else:
-        w += 1
+        w += cur_n_blocks
 
-    seen[nr] = random.randint(0, 65535)
-    b = gen_block(blocksize, offset, seen[nr])
+    b = bytearray()
+    for i in range(0, cur_n_blocks):
+        seen[nr + i] = random.randint(0, 65535)
+        b += gen_block(blocksize, offset + i * blocksize, seen[nr + i])
     os.pwrite(fd, b, offset)
 
     n += 1
+    total_n += cur_n_blocks
 
     now = time.time()
     if now - prev >= 1:
-        print(f'total: {n}, n/s: {int(n / (now - start))}, percent done: {w * 100 / n_blocks:.2f}, n verified: {verified}')
+        print(f'total: {n}, n/s: {int(n / (now - start))}, avg block count per iteration: {total_n / n:.2f}, percent done: {w * 100 / n_blocks:.2f}, n verified: {verified}')
         prev = now
