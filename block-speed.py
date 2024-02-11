bb5f1a0069a3807b9d9ef75ce7efd06bdb065920
folkert van heusden <mail@vanheusden.com>
2024-02-11 03:10:46 +0100

    generate 3D plots

--- block-speed.py
+++ block-speed.py
@@ -3,28 +3,61 @@
 import os
 import random
 import sys
+import threading
 import time
 
-dev=sys.argv[1]
-bs=1
-dt=5
+dev = sys.argv[1]
+bs_depth = int(sys.argv[2])
+jobs_depth = int(sys.argv[3])
+duration = int(sys.argv[4])
 
-fd = os.open(dev, os.O_RDONLY)
-dev_size = os.lseek(fd, 0, os.SEEK_END)
-
-for m in range(20):
-    bs *= 2
-
-    n = 0
-
+def do_(duration, bs, results, results_index):
+    fd = os.open(dev, os.O_RDONLY)
+    dev_size = os.lseek(fd, 0, os.SEEK_END)
     start = time.time()
+    iop = 0
+    took = 0
     while True:
-        now = time.time()
-        if now >= start + dt:
-            break
         offset = random.randint(0, dev_size) & ~(bs - 1)
         os.lseek(fd, offset, os.SEEK_SET)
         os.read(fd, bs)
-        n += 1
 
-    print(m, bs, n * dt / (now - start))
+        iop += 1
+
+        now = time.time()
+        if now >= start + duration / 1000.:
+            took = now - start
+            break
+
+    results[results_index] = iop * duration / (took * 1000.)
+
+    os.close(fd)
+
+fh = open('plot.dat', 'w')
+
+for bs_it in range(0, bs_depth):
+    bs = 512 << bs_it
+
+    for jd_it in range(0, jobs_depth):
+        temp_jd_nr = jd_it + 1
+        threads = [ None ] * temp_jd_nr
+        results = [ None ] * temp_jd_nr
+
+        for t in range(0, temp_jd_nr):
+            th = threading.Thread(target=do_, args=(duration, bs, results, t))
+            th.start()
+            threads[t] = th
+
+        total_iops = 0
+        for t in range(0, temp_jd_nr):
+            threads[t].join()
+            total_iops += results[t]
+
+        str_ = f'{bs}\t{temp_jd_nr}\t{total_iops}'
+        print(str_)
+        fh.write(f'{str_}\n')
+
+    fh.write('\n')
+    print()
+
+fh.close()
