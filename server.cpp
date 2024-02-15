folkertvanheusden/iESP

commit b02078db242e9e0ad2c97ca321fbfde9861f97e7

tweaks to allow vmware to go further for using a store - it now halts on ATS

server.cpp
@@ -180,7 +180,7 @@ bool server::push_response(com_client *const cc, session *const ses, iscsi_pdu_b
 		auto     pdu_data_out = reinterpret_cast<iscsi_pdu_scsi_data_out *>(pdu);
 		uint32_t offset       = pdu_data_out->get_BufferOffset();
 		auto     data         = pdu_data_out->get_data();
-		uint32_t TTT          = pdu_data_out->get_TTT();
+		uint32_t TTT          = pdu_data_out->get_Itasktag();  // this software uses the ITT as TTT
 		bool     F            = pdu_data_out->get_F();
 		auto     session      = ses->get_r2t_sesion(TTT);
 

session.cpp
@@ -29,21 +29,21 @@ uint32_t session::get_inc_datasn(const uint32_t data_sn_itt)
 
 uint32_t session::init_r2t_session(const r2t_session & rs, iscsi_pdu_scsi_cmd *const pdu)
 {
-	uint32_t TTT = pdu->get_Itasktag();
+	uint32_t ITT = pdu->get_Itasktag();
 
-	auto it = r2t_sessions.find(TTT);
+	auto it = r2t_sessions.find(ITT);
 	if (it != r2t_sessions.end())
-		return TTT;
+		return ITT;
 
 	r2t_session *copy = new r2t_session;
 	*copy = rs;
 
 	copy->PDU_initiator = pdu->get_raw();
 
-	DOLOG("session::init_r2t_session: register TTT %08x\n", TTT);
-	r2t_sessions.insert({ TTT, copy });
+	DOLOG("session::init_r2t_session: register ITT %08x\n", ITT);
+	r2t_sessions.insert({ ITT, copy });
 
-	return TTT;
+	return ITT;
 }
 
 r2t_session *session::get_r2t_sesion(const uint32_t ttt)

commit 5d91419d337b7a6984049194850b05bca0edb711

b02078db242e9e0ad2c97ca321fbfde9861f97e7 broke at R2T

server.cpp
@@ -180,9 +180,13 @@ bool server::push_response(com_client *const cc, session *const ses, iscsi_pdu_b
 		auto     pdu_data_out = reinterpret_cast<iscsi_pdu_scsi_data_out *>(pdu);
 		uint32_t offset       = pdu_data_out->get_BufferOffset();
 		auto     data         = pdu_data_out->get_data();
-		uint32_t TTT          = pdu_data_out->get_Itasktag();  // this software uses the ITT as TTT
+		uint32_t TTT          = pdu_data_out->get_TTT();
 		bool     F            = pdu_data_out->get_F();
 		auto     session      = ses->get_r2t_sesion(TTT);
+
+		if (TTT == 0xffffffff) {  // unsollicited data
+			TTT = pdu_data_out->get_Itasktag();
+		}
 
 		if (session == nullptr) {
 			DOLOG("server::push_response: DATA-OUT PDU references unknown TTT (%08x)\n", TTT);