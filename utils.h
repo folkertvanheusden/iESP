4873c268206880f30809132ebcc03f0d25e81c5d
folkert van heusden <mail@vanheusden.com>
2024-03-01 21:30:27 +0100

    REPORT_LUNS fix

--- utils.h
+++ utils.h
@@ -10,6 +10,7 @@ uint32_t get_free_heap_space();
 uint64_t get_uint64_t(const uint8_t *const p);
 uint32_t get_uint32_t(const uint8_t *const p);
 uint64_t get_micros();
+void encode_lun(uint8_t *const target, const uint64_t lun_nr);
 
 #if !defined(ARDUINO)
 std::string to_hex(const uint8_t *const in, const size_t n);
