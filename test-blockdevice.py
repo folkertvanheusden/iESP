a0d31a9bea05639275d0d2d82dfc92d2d93fcf44
Folkert van Heusden <mail@vanheusden.com>
2024-09-18 14:51:51 +0200

    - explanation of parameters - datasync instead of meta+data sync - source code comments

--- test-blockdevice.py
+++ test-blockdevice.py
@@ -10,7 +10,13 @@ import time
 
 if len(sys.argv) != 5:
     print(f'Usage: {sys.argv[0]} dev blocksize maxblockcount unique-percentage')
+    print('dev:               block device')
+    print('blocksize:         e.g. 512 or 4096')
+    print('maxblockcount:     maximum number of blocks to write in one go')
+    print('unique-percentage: for testing de-duplication devices')
+    print()
     print(' ##### DO NOT RUN THIS ON A DEVICE WITH DATA! IT GETS ERASED! ###### ')
+    print()
     sys.exit(1)
 
 dev = sys.argv[1]  # device file
@@ -25,6 +31,8 @@ fd = os.open(dev, os.O_RDWR | os.O_DIRECT)
 dev_size = os.lseek(fd, 0, os.SEEK_END)
 
 n_blocks = dev_size // blocksize
+if dev_size % blocksize:
+    print(f'Note: disk is not a multiple of {blocksize} in size ({dev_size})!')
 seen = [ None ] * n_blocks
 
 duplicate = 0xffffffff
@@ -49,10 +57,13 @@ prev = start
 w = 0
 
 while True:
+    # pick a number of blocks to work on
     cur_n_blocks = random.randint(1, max_b)
+    # pick an offset (nr) in the device
     offset = random.randint(0, dev_size - blocksize * cur_n_blocks) & ~(blocksize - 1)
     nr = offset // blocksize
 
+    # have all 'cur_n_blocks' starting at 'nr' been written to?
     b = []
     has_none = False
     for i in range(0, cur_n_blocks):
@@ -60,13 +71,14 @@ while True:
             has_none = True
             break
 
-    if has_none == False:
+    if has_none == False:  # yes(!)
         # verify
         for i in range(0, cur_n_blocks):
             b.append(gen_block(blocksize, offset + i * blocksize, seen[nr + i]))
             if seen[nr + i] == duplicate:
                 verified_d += 1
 
+        # read from disk
         data = os.pread(fd, blocksize * cur_n_blocks, offset)
 
         for i in range(0, cur_n_blocks):
@@ -78,13 +90,17 @@ while True:
     else:
         w += cur_n_blocks
 
+    # update blocks with new data
     b = bytearray()
     for i in range(0, cur_n_blocks):
+        # choose new seed. if not duplicate (e.g. not de-dubeable), use a random
         new_seen = duplicate if random.randint(0, 100) > unique_perc else random.randint(0, 65535)
+        # remember the seed
         seen[nr + i] = new_seen
+        # generate & add block of semi(!)-random data
         b += gen_block(blocksize, offset + i * blocksize, seen[nr + i])
     os.pwrite(fd, b, offset)
-    os.fsync(fd)
+    os.fdatasync(fd)
 
     n += 1
     total_n += cur_n_blocks
