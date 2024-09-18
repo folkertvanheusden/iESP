208877155227900806e1c875d2e14cb4c7f69121
Folkert van Heusden <mail@vanheusden.com>
2024-09-18 16:10:39 +0200

    threads

--- test-blockdevice.py
+++ test-blockdevice.py
@@ -4,10 +4,12 @@ import hashlib
 import os
 import random
 import sys
+import threading
 import time
 
 hash_algo = hashlib.sha3_512
 fast_random = True
+n_threads = 6
 
 ##### DO NOT RUN THIS ON A DEVICE WITH DATA! IT GETS ERASED! ######
 
@@ -40,6 +42,7 @@ seen = [ None ] * n_blocks
 
 duplicate = 0xffffffff
 
+random.seed()  # different from random.Random
 rnd = random.Random()
 
 def gen_block(size, offset, seed2):
@@ -58,74 +61,92 @@ def gen_block(size, offset, seed2):
             m = hash_algo(m).digest()
     return out
 
+start = time.time()
 total_n = 0
 n = 0
 verified = 0
 verified_d = 0
-start = time.time()
-prev = start
 w = 0
 read_error_count = 0
 write_error_count = 0
 
-while True:
-    # pick a number of blocks to work on
-    cur_n_blocks = random.randint(1, max_b)
-    # pick an offset (nr) in the device
-    offset = random.randint(0, dev_size - blocksize * cur_n_blocks) & ~(blocksize - 1)
-    nr = offset // blocksize
+def do():
+    global total_n
+    global n
+    global verified
+    global verified_d
+    global w
+    global read_error_count
+    global write_error_count
 
-    # have all 'cur_n_blocks' starting at 'nr' been written to?
-    b = []
-    has_none = False
-    for i in range(0, cur_n_blocks):
-        if seen[nr + i] == None:
-            has_none = True
-            break
+    prev = start
 
-    if has_none == False:  # yes(!)
-        # verify
+    while True:
+        # pick a number of blocks to work on
+        cur_n_blocks = random.randint(1, max_b)
+        # pick an offset (nr) in the device
+        offset = random.randint(0, dev_size - blocksize * cur_n_blocks) & ~(blocksize - 1)
+        nr = offset // blocksize
+
+        # have all 'cur_n_blocks' starting at 'nr' been written to?
+        b = []
+        has_none = False
         for i in range(0, cur_n_blocks):
-            b.append(gen_block(blocksize, offset + i * blocksize, seen[nr + i]))
-            if seen[nr + i] == duplicate:
-                verified_d += 1
+            if seen[nr + i] == None:
+                has_none = True
+                break
 
-        # read from disk
-        try:
-            data = os.pread(fd, blocksize * cur_n_blocks, offset)
-
+        if has_none == False:  # yes(!)
+            # verify
             for i in range(0, cur_n_blocks):
-                cur_b_offset = i * blocksize 
-                assert(data[cur_b_offset:cur_b_offset+blocksize] == b[i])
-                verified += 1
+                b.append(gen_block(blocksize, offset + i * blocksize, seen[nr + i]))
+                if seen[nr + i] == duplicate:
+                    verified_d += 1
 
+            # read from disk
+            try:
+                data = os.pread(fd, blocksize * cur_n_blocks, offset)
+
+                for i in range(0, cur_n_blocks):
+                    cur_b_offset = i * blocksize 
+                    assert(data[cur_b_offset:cur_b_offset+blocksize] == b[i])
+                    verified += 1
+
+            except OSError as e:
+                print(f'Read error: {e} at {offset} ({len(b)} bytes)', offset/blocksize)
+                read_error_count += 1
+
+        else:
+            w += cur_n_blocks
+
+        # update blocks with new data
+        b = bytearray()
+        for i in range(0, cur_n_blocks):
+            # choose new seed. if not duplicate (e.g. not de-dubeable), use a random
+            new_seen = duplicate if random.randint(0, 100) > unique_perc else random.randint(0, 65535)
+            # remember the seed
+            seen[nr + i] = new_seen
+            # generate & add block of semi(!)-random data
+            b += gen_block(blocksize, offset + i * blocksize, seen[nr + i])
+        try:
+            os.pwrite(fd, b, offset)
+            os.fdatasync(fd)
         except OSError as e:
-            print(f'Read error: {e} at {offset} ({len(b)} bytes)', offset/blocksize)
-            read_error_count += 1
+            print(f'Write error: {e} at {offset} ({len(b)} bytes)', offset/blocksize)
+            write_error_count += 1
 
-    else:
-        w += cur_n_blocks
+        n += 1
+        total_n += cur_n_blocks
 
-    # update blocks with new data
-    b = bytearray()
-    for i in range(0, cur_n_blocks):
-        # choose new seed. if not duplicate (e.g. not de-dubeable), use a random
-        new_seen = duplicate if random.randint(0, 100) > unique_perc else random.randint(0, 65535)
-        # remember the seed
-        seen[nr + i] = new_seen
-        # generate & add block of semi(!)-random data
-        b += gen_block(blocksize, offset + i * blocksize, seen[nr + i])
-    try:
-        os.pwrite(fd, b, offset)
-        os.fdatasync(fd)
-    except OSError as e:
-        print(f'Write error: {e} at {offset} ({len(b)} bytes)', offset/blocksize)
-        write_error_count += 1
+        now = time.time()
+        if now - prev >= 1:
+            print(f'total: {n}, n/s: {int(n / (now - start))}, avg block count per iteration: {total_n / n:.2f}, percent done: {w * 100 / n_blocks:.2f}, n verified: {verified}/{verified_d}, write errors: {write_error_count}')
+            prev = now
 
-    n += 1
-    total_n += cur_n_blocks
+t = []
+for i in range(n_threads):
+    tcur = threading.Thread(target=do)
+    tcur.start()
+    t.append(tcur)
 
-    now = time.time()
-    if now - prev >= 1:
-        print(f'total: {n}, n/s: {int(n / (now - start))}, avg block count per iteration: {total_n / n:.2f}, percent done: {w * 100 / n_blocks:.2f}, n verified: {verified}/{verified_d}, write errors: {write_error_count}')
-        prev = now
+t[0].join()
